736edea5604f33e12c07b074cda8cb2b13106200
atomirex <nige@atomirex.com>
2023-05-31 19:29:51 -0400

    First

---
+++ README.md
@@ -0,0 +1,37 @@
+# fishtank-js
+Provides a webcam driven parallax effect simulating the feeling of looking through the screen into a fishtank, with the perspective adjustments driven by face detection from the webcam. Could be fairly easily repurposed to any three.js scene.
+
+Very inspired by the work of [Johnny Lee with the head tracking using the Wii sensor bar](http://johnnylee.net/projects/wii/) I hacked up a proof of concept for doing this on the web using no extra hardware and just [Google MediaPipe](https://developers.google.com/mediapipe) and the 3D graphics library [three.js](https://threejs.org). The result is an unholy fusion of mediapipe face detection, three.js example code and some modified mathematics originally by Mr Lee. It is definitely not production ready, largely due to typical media lifecycle problems, but does show that this is viable.
+
+The actual core code here is amazingly short.
+
+## Running it
+The easiest way is to check this link to the running example: (TO BE ADDED)
+
+### The settings
+The more accurate your settings the better the effect.
+- "FOV (degrees)" - the field of vision, horizontally, of the camera in use, in degrees. Note the internal API uses radians.
+- "Above screen" - if the camera is directly above the screen or directly below. Cameras in other locations are not supported.
+- "Height (mm)" - the height of the display window in millimetres. (The width is then calculated via the aspect ratio).
+- "Inter eye distance (mm)" - the distance between the eyes of the viewer. This is equivalent to the distance between the two dots in the earlier work with the Wii hardware.
+
+### Local build and development
+1. Clone this repo
+2. `npm install`
+3. `npm run dev`
+4. Open your browser to the appropriate page
+5. Shut your browser again, and open that same page in a new window manually. (This is because the auto refresh code of vite interacts badly with some of the initialization going on here).
+6. Move the window to the screen your camera is near
+7. Select the camera from the drop down (the other settings can be changed after starting).
+8. Notice the parallax effect when you move your head around. Left/right, up/down and in/out will all work. (In the example you can move behind one of the cubes).
+9. Break it in some unknown way. Create a bug report and move on with life.
+
+## Known problems
+- It's going to be a performance monster, but runs well on current Macs
+- The maths is likely to have subtle bugs and precision errors.
+- Cameras are either in the centre above or below the screen. No consideration for other configurations.
+- Once set at the start the current camera is unalterable. Some attention to teardown and recreation of the streams and so on is required.
+- The interactions with the reloading mechanism of the dev server get old fast. Whenever you save a file it will break any current active session.
+- I would want to break out the classes HeadTracker and FishtankCamera into another module which could then go on npm with clearing versioning and licensing etc. The example uses an hdr env map from three.js which is cheeky to say the least and should be changed.
+- The results of the face detection are shaky. It may be possible to filter them for some applications, but that would introduce latency. It is probably better to hope for improvements in resolution and accuracy in the face detection as things like WebGPU get deployed and that becomes more reasonable. Relatedly, the range in distance from the camera is currently limited. There may be good mileage from switching to a pose detection mechanism, depending on the stability of the output of the pose inference.
+- If adapting the maths for a native mobile app make sure to use native inference APIs where available as the difference is dramatic.
---
+++ src/script.js
@@ -0,0 +1,402 @@
+import * as THREE from 'three';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+import {
+    FaceDetector,
+    FilesetResolver,
+} from "@mediapipe/tasks-vision";
+
+const DEG2RAD = (Math.PI * 2) / 360.0;
+
+class HeadTracker {
+    constructor(videoElement) {
+        this.videoElement = videoElement; // Where the camera feed comes from
+        this.faceDetector = null;
+        this.running = false;
+        this.lastVideoTime = -1;
+
+        this.onDetected = null;
+
+        // Starting assumptions for all settings
+        this.settings = {
+            screenHeightMM: 180,
+            interEyeDistanceMM: 100, // Distance between the eyes
+
+            cameraDeviceId: null,
+            cameraFov: DEG2RAD * 40,
+            cameraAboveScreen: true,
+            cameraVerticalAngle: 0.0,
+            cameraAspectRatio: 16.0 / 9.0,
+        };
+    }
+
+    // This has to be called before starting
+    setCameraDeviceId(cameraDeviceId) {
+        this.settings.cameraDeviceId = cameraDeviceId;
+    }
+
+    setScreenHeightMM(screenHeightMM) {
+        this.settings.screenHeightMM = screenHeightMM;
+    }
+
+    setInterEyeDistanceMM(interEyeDistanceMM) {
+        this.settings.interEyeDistanceMM = interEyeDistanceMM;
+    }
+
+    setCameraFov(cameraFov) {
+        this.settings.cameraFov = cameraFov;
+    }
+
+    setCameraAboveScreen(cameraAboveScreen) {
+        this.settings.cameraAboveScreen = cameraAboveScreen;
+    }
+
+    async detect() {
+        if (!this.running) {
+            this.running = true;
+            await this.faceDetector.setOptions({ runningMode: "VIDEO" });
+        }
+
+        let video = this.videoElement;
+    
+        let startTimeMs = performance.now();
+    
+        if (video.currentTime !== this.lastVideoTime) {
+            this.lastVideoTime = video.currentTime;
+            const detections = this.faceDetector.detectForVideo(video, startTimeMs).detections;
+    
+            let found = null;
+            for (let d of detections) {
+                let confidence = d.categories[0].score;
+    
+                if (confidence > 0.7) {
+                    if (found == null) {
+                        found = d;
+                    } else if (confidence > found.categories[0].score) {
+                        found = d;
+                    }
+                }
+            }
+    
+            if (found != null) {
+                if (found.keypoints.length > 1) {
+                    const {
+                        screenHeightMM, 
+                        interEyeDistanceMM, 
+                        cameraFov, 
+                        cameraAboveScreen,
+                        cameraVerticalAngle,
+                        cameraAspectRatio,
+                    } = this.settings;
+
+                    const cameraRadiansPerUnit = (cameraFov / cameraAspectRatio);
+    
+                    // Get the eyes! They're at 0 and 1!
+                    const l = found.keypoints[0];
+                    const r = found.keypoints[1];
+    
+                    const dx = (l.x - r.x) * cameraAspectRatio;
+                    const dy = l.y - r.y;
+                    const interEyeDistance = Math.sqrt((dx * dx) + (dy * dy));
+    
+                    const angle = cameraRadiansPerUnit * interEyeDistance / 2.0;
+    
+                    const z = ((interEyeDistanceMM / 2.0) / Math.tan(angle)) / screenHeightMM;
+    
+                    const cx = ((l.x + r.x) / 2.0) * cameraAspectRatio;
+                    const cy = (l.y + r.y) / 2.0;
+    
+                    const x = Math.sin(cameraRadiansPerUnit * (cx - (0.5 * cameraAspectRatio))) * z;
+                    const relativeVerticalAngle = (cy - 0.375) * cameraRadiansPerUnit;
+    
+                    let y = (cameraAboveScreen ? -1.0 : 1.0) * ((Math.sin(relativeVerticalAngle + cameraVerticalAngle)) * z);
+                    
+                    if(this.onDetected != null) {
+                        this.onDetected(x, y, z);
+                    }
+                }
+            }
+        }
+    
+        var that = this;
+        window.requestAnimationFrame(function() {
+            that.detect();
+        });   
+    }
+    
+    async start() {
+        //
+        // This totally isn't production ready, largely because of this section.
+        //
+        // Some much cleaner disposal/recreation is necessary
+        //
+        var that = this;
+
+        const visionTasks = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+
+        this.faceDetector = await FaceDetector.createFromOptions(visionTasks, {
+            baseOptions: {
+                modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
+                delegate: "GPU"
+            },
+            runningMode: "IMAGE"
+        });
+
+        const constraints = {
+            video: (that.settings.cameraDeviceId == null ? true : {deviceId: that.settings.cameraDeviceId})
+        };
+    
+        navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(function (stream) {
+                that.mediaStream = stream;
+
+                that.videoElement.srcObject = stream;
+                that.videoElement.addEventListener("loadeddata", function() {
+                    that.detect();
+                });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+}
+
+const headTracker = new HeadTracker(document.getElementById("camera"));
+
+class FishtankCamera extends THREE.PerspectiveCamera {
+    constructor(fov = 50, aspect = 1, near = 0.1, far = 2000) {
+        super(fov, aspect, near, far);
+
+        this.target = new THREE.Vector3(0.0, 0.0, 0.0);
+        this.up = new THREE.Vector3(0.0, 1.0, 0.0);
+    }
+
+    setViewerPosition(x, y, distance) {
+        this.viewerPosition = {x: x, y: y, z: distance};
+        this.updateProjectionMatrix();
+    }
+
+    updateProjectionMatrix() {
+        if(this.viewerPosition) {
+            const near = this.near;
+            const {x, y, z} = this.viewerPosition;
+
+            // Make a projection based on the position
+            // This is an adaptation of the parameters used by Johnny Lee in the head tracking 
+            // http://johnnylee.net/projects/wii/
+            this.projectionMatrix.makePerspective(
+                near * (-1.0 * this.aspect + x) / z, 
+                near * (1.0 * this.aspect + x) / z, 
+                near * (1.0 - y) / z, 
+                near * (-1.0 - y) / z,
+                near, 
+                this.far
+            );
+
+            this.position.x = -x;
+            this.position.y = y;
+            this.position.z = z;
+
+            this.target.set(x, y, 0.0);
+            
+            this.matrixWorld.lookAt(this.position, this.target, this.up);
+            this.matrixWorldInverse.copy(this.matrixWorld);
+            this.matrixWorldInverse.invert();
+
+            this.projectionMatrixInverse.copy(this.projectionMatrix).invert();
+        } else {
+            super.updateProjectionMatrix();
+        }
+    }
+}
+
+function setupControls() {
+    let mediaEnabled = !!navigator.mediaDevices?.getUserMedia;
+    if(!mediaEnabled) {
+        alert("The browser isn't allowing usermedia access so it won't work");
+        return;
+    }
+
+    const cameraSelector = document.getElementById("camera-selector");
+    cameraSelector.addEventListener("change", function() {
+        headTracker.setCameraDeviceId(cameraSelector.value);
+    });
+    cameraSelector.disabled = true;
+
+    const startButton = document.getElementById("start-button");
+    startButton.addEventListener("click", function() {
+        startButton.disabled = true;
+        cameraSelector.disabled = true;
+        headTracker.start();
+    });
+
+    const cameraFovDegInput = document.getElementById("camera-fov-deg");
+    cameraFovDegInput.addEventListener("change", function() {
+        headTracker.setCameraFov(parseFloat(cameraFovDegInput.value) * DEG2RAD);
+    });
+
+    const cameraAboveScreenInput = document.getElementById("camera-above-screen");
+    cameraAboveScreenInput.addEventListener("change", function() {
+        headTracker.setCameraAboveScreen(cameraAboveScreenInput.checked);
+    });
+
+    const displayHeightMMInput = document.getElementById("display-height-mm");
+    displayHeightMMInput.addEventListener("change", function() {
+        headTracker.setScreenHeightMM(parseFloat(displayHeightMMInput.value));
+    });
+
+    const viewerInterEyeDistance = document.getElementById("viewer-inter-eye-distance-mm");
+    viewerInterEyeDistance.addEventListener("change", function() {
+        headTracker.setInterEyeDistanceMM(parseFloat(viewerInterEyeDistance.value));
+    });
+
+    navigator.mediaDevices
+        .getUserMedia({video: true})
+        .then(() => {
+            navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+                const cameras = devices.filter(device => device.kind == 'videoinput');
+    
+                cameraSelector.innerHTML = '';
+    
+                cameras.forEach(camera => {
+                    const opt = document.createElement('option');
+                    opt.value = camera.deviceId;
+                    opt.text = camera.label || camera.deviceId;
+                    cameraSelector.appendChild(opt);
+                });
+
+                cameraSelector.disabled = false;
+
+                alert("Please select your camera and press 'START'");
+                startButton.disabled = false;
+            })
+            .catch(error => {
+                startButton.disabled = true;
+                alert("There was an error getting your cameras, so this won't work");
+            });
+        })
+        .catch((err) => {
+            startButton.disabled = true;
+            alert("There was an error getting your cameras, so this won't work");
+        });
+}
+
+export async function start() {
+    setupControls();
+
+    const canvas = document.getElementById("canvas");
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+
+    const scene = new THREE.Scene();
+
+    // Evil access of the three js hdr env map
+    new RGBELoader()
+        .setPath( 'https://threejs.org/examples/textures/equirectangular/' )
+        .load( 'quarry_01_1k.hdr', function ( texture ) {
+            texture.mapping = THREE.EquirectangularReflectionMapping;
+
+            // scene.background = texture;
+            // Replace the standard bg sphere with one further away
+            const geometry = new THREE.SphereGeometry(900, 60, 60);
+            geometry.scale(-1, 1, 1); // Invert the normals
+            const material = new THREE.MeshBasicMaterial({ map: texture});
+            const sphere = new THREE.Mesh(geometry, material);
+            scene.add(sphere);
+
+            scene.environment = texture;
+        } );
+
+    const projectionCamera = new FishtankCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
+    projectionCamera.position.z = 5;
+
+    headTracker.onDetected = function(x, y, z) {
+        projectionCamera.setViewerPosition(x, y, z);
+    };
+
+    const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 256 );
+    cubeRenderTarget.texture.type = THREE.HalfFloatType;
+
+    const cubeCamera = new THREE.CubeCamera( 1, 1000, cubeRenderTarget );
+    const reflective = new THREE.MeshStandardMaterial( {
+        color: 0xee0044,
+        envMap: cubeRenderTarget.texture,
+        roughness: 0.05,
+        metalness: 0.0,
+        clearcoat: 0.2,
+        clearcoatRoughness: 0.05,
+    } );    
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshPhysicalMaterial({
+        color: 0xcccccc,
+        roughness: 0.1,
+        metalness: 0.0,
+        clearcoat: 0.2,
+        clearcoatRoughness: 0.05,
+        transmission: 0.0
+    });
+
+    const cubeDefs = [
+        [0,0,0, 1.0],
+        [-2, 0, -8, 1.0],
+        [2, 0, -2, 1.0],
+        [2, 0, -8, 1.0],
+        [3, -2, -5, 1.0],
+        [0.15, 0.15, 5.0, 0.25], // Sticking out
+        [0.0, -1.5, -0.5, 1.0], // Floor
+        [0.0, 1.5, -0.5, 1.0], // Ceiling
+    ];
+
+    let cubes = [];
+    for(var i=0; i<cubeDefs.length; i++) {
+        const def = cubeDefs[i];
+
+        if(i == 0) {
+            const torus = new THREE.Mesh( new THREE.TorusKnotGeometry(0.5, 0.17, 128, 16), reflective);
+            scene.add( torus );
+
+            torus.scale.x = def[3];
+            torus.scale.y = def[3];
+            torus.scale.z = def[3];
+
+            cubes.push(torus);
+        } else {
+            const cube = new THREE.Mesh(geometry, material);
+            scene.add(cube);
+            cube.position.x = def[0];
+            cube.position.y = def[1];
+            cube.position.z = def[2];
+
+            cube.scale.x = def[3];
+            cube.scale.y = def[3];
+            cube.scale.z = def[3];
+
+            cubes.push(cube);
+        }
+    } 
+
+    // Left and right would be doable, but need to move when the aspect ratio changes!!!!
+
+    function resized() {
+        projectionCamera.aspect = window.innerWidth / window.innerHeight;
+        projectionCamera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener("resize", resized);
+    resized();
+
+    function step() {
+        requestAnimationFrame(step);
+        cubes[0].rotation.x += 0.01;
+        cubes[0].rotation.y += 0.01;
+        cubeCamera.update(renderer, scene);
+        renderer.render(scene, projectionCamera);
+    }
+
+    step();
+}
+
+document.body.onload = start;
